a7a636e5685461e1f482742500f643fa609b2e68
Suraj B M <silicoflare@gmail.com>
2024-03-05 15:58:15 +0530

    Fix card size on mobile

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ export type CardProps = {
 
 export const Card = ({ heading, children } : CardProps) => {
     return (
-        <div className="p-3 rounded-md border-2 border-gray-600 flex flex-col items-center justify-start h-20 md:h-40 hover:scale-105 transition ease-in-out duration-300">
+        <div className="p-3 rounded-md border-2 border-gray-600 flex flex-col items-center justify-start min-h-30 md:min-h-40 hover:scale-105 transition ease-in-out duration-300">
             <span className="font-['Roboto_Slab'] font-semibold text-xl">
                 { heading }
             </span>
